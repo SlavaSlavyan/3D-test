459032dcd57104c796292b7df7c85b841723baf5
Svyatoslav <tortik2000.g@gmail.com>
2025-05-26 14:10:08 +0300

    0.0.0

---
+++ function/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Main:
+    
+    def __init__(self):
+        
+        pygame.init()
+        
+    def start(self):
+        
+        while True:
+            
+            pass
